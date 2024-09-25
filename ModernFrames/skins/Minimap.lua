89b0fb5cde32d30af8080513e6d735391e000b3c
Daenarys <androidwoz@gmail.com>
2024-09-25 15:42:29 +0200

    adjust df button pos

--- ModernFrames/skins/Minimap.lua
+++ ModernFrames/skins/Minimap.lua
@@ -1,5 +1,21 @@
 if (C_AddOns.IsAddOnLoaded("SexyMap")) then return end
 
+Minimap:HookScript("OnEvent", function(self, event, ...)
+	if ( event == "PLAYER_ENTERING_WORLD" ) then
+		if (ExpansionLandingPageMinimapButton:GetNormalTexture():GetAtlas() == "dragonflight-landingbutton-up") then
+			ExpansionLandingPageMinimapButton:ClearAllPoints()
+			ExpansionLandingPageMinimapButton:SetPoint("TOPLEFT", 8, -156)
+		end
+
+		hooksecurefunc(ExpansionLandingPageMinimapButton, "UpdateIcon", function(self)
+			if (self:GetNormalTexture():GetAtlas() == "dragonflight-landingbutton-up") then
+				self:ClearAllPoints()
+				self:SetPoint("TOPLEFT", 8, -156)
+			end
+		end)
+	end
+end)
+
 hooksecurefunc(QueueStatusButton, "UpdatePosition", function(self)
 	self:SetParent(MinimapBackdrop)
 	self:SetFrameLevel(6)
@@ -11,8 +27,4 @@ end)
 hooksecurefunc(QueueStatusFrame, "UpdatePosition", function(self)
 	self:ClearAllPoints()
 	self:SetPoint("TOPRIGHT", QueueStatusButton, "TOPLEFT")
-end)
-
-hooksecurefunc(AddonCompartmentFrame, "UpdateDisplay", function(self)
-	self:SetShown(false)
 end)
