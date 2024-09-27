8319902c1b9d75d65a401e592c8aeb00909b6a0f
Daenarys <androidwoz@gmail.com>
2024-09-27 16:01:19 +0200

    update those role icons

--- ModernFrames/skins/LFG.lua
+++ ModernFrames/skins/LFG.lua
@@ -40,3 +40,32 @@ hooksecurefunc("GroupFinderFrame_EvaluateButtonVisibility", function(self)
 		self.groupButton3:SetPoint("TOP", self.groupButton2, "BOTTOM", 0, -30)
 	end
 end)
+
+hooksecurefunc('LFGListGroupDataDisplayEnumerate_Update', function(self, numPlayers, displayData, disabled, iconOrder)
+	local LFG_LIST_GROUP_DATA_ATLASES = {
+		["TANK"] = "UI-LFG-RoleIcon-Tank-Micro-GroupFinder",
+		["HEALER"] = "UI-LFG-RoleIcon-Healer-Micro-GroupFinder",
+		["DAMAGER"] = "UI-LFG-RoleIcon-DPS-Micro-GroupFinder",
+	};
+
+	--Note that icons are numbered from right to left
+	if iconOrder == LFG_LIST_GROUP_DATA_ROLE_ORDER then
+		local iconIndex = numPlayers;
+		for i=1, #iconOrder do
+			local role = iconOrder[i];
+			for j=1, displayData[iconOrder[i]] do
+				local icon = self.Icons[iconIndex];
+				icon:SetAlpha(disabled and 0.5 or 0.70);
+				icon.RoleIconWithBackground:Hide()
+				icon.RoleIcon:Show()
+				icon.RoleIcon:SetSize(17, 17)
+				icon.RoleIcon:SetAtlas(LFG_LIST_GROUP_DATA_ATLASES[role], false)
+				icon.ClassCircle:Hide()
+				iconIndex = iconIndex - 1;
+				if ( iconIndex < 1 ) then
+					return;
+				end
+			end
+		end
+	end
+end)
