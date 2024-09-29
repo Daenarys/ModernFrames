169077b9f4a77a1369b0d69573187e6d2ab5d4af
Daenarys <androidwoz@gmail.com>
2024-09-29 23:50:40 +0200

    give bosses their golden wings

--- ModernFrames/skins/unitframes/TargetFrame.lua
+++ ModernFrames/skins/unitframes/TargetFrame.lua
@@ -3,7 +3,10 @@ if not _G.TargetFrame then return end
 hooksecurefunc(TargetFrame, "CheckClassification", function(self)
 	local classification = UnitClassification(self.unit)
 	local bossPortraitFrameTexture = self.TargetFrameContainer.BossPortraitFrameTexture
-	if (UnitIsBossMob(self.unit) or classification == "elite" ) then
+	if (UnitIsBossMob(self.unit) and not UnitIsFriend("player", self.unit)) then
+		bossPortraitFrameTexture:SetAtlas("UI-HUD-UnitFrame-Target-PortraitOn-Boss-Gold-Winged", TextureKitConstants.UseAtlasSize)
+		bossPortraitFrameTexture:SetPoint("TOPRIGHT", 8, -7)
+	elseif (UnitIsBossMob(self.unit) and UnitIsFriend("player", self.unit) or classification == "elite") then
 		bossPortraitFrameTexture:SetAtlas("UI-HUD-UnitFrame-Target-PortraitOn-Boss-Gold", TextureKitConstants.UseAtlasSize)
 		bossPortraitFrameTexture:SetPoint("TOPRIGHT", -11, -8)
 	elseif (classification == "rare") then
