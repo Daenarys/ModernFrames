f84e11448ea2d3c2f5157b03517ef8267b70c605
Daenarys <androidwoz@gmail.com>
2024-09-30 11:43:06 +0200

    move out of update

--- ModernFrames/skins/ObjectiveTracker.lua
+++ ModernFrames/skins/ObjectiveTracker.lua
@@ -1,17 +1,25 @@
 if not _G.ObjectiveTrackerFrame then return end
 
-hooksecurefunc(ObjectiveTrackerFrame, "Update", function(self)
-	if not self.Header.MinimizeButton.IsSkinned then
-		self.Header.MinimizeButton:SetNormalTexture("Interface/QuestFrame/QuestTracker")
-		self.Header.MinimizeButton:SetPushedTexture("Interface/QuestFrame/QuestTracker")
-		self.Header.MinimizeButton:SetHighlightAtlas("UI-QuestTrackerButton-Highlight", "ADD")
-		if self:IsCollapsed() then
-			self.Header.MinimizeButton:GetNormalTexture():SetTexCoord(0.933594, 0.96875, 0.242188, 0.316406)
-			self.Header.MinimizeButton:GetPushedTexture():SetTexCoord(0.894531, 0.929688, 0.324219, 0.398438)
-		else
-			self.Header.MinimizeButton:GetNormalTexture():SetTexCoord(0.9375, 0.972656, 0.121094, 0.195312)
-			self.Header.MinimizeButton:GetPushedTexture():SetTexCoord(0.894531, 0.929688, 0.242188, 0.316406)
-		end
-		self.Header.MinimizeButton.IsSkinned = true
+local TrackerFrame = _G.ObjectiveTrackerFrame
+local TrackerHeader = TrackerFrame and TrackerFrame.Header
+local MinimizeButton = TrackerFrame.Header and TrackerHeader.MinimizeButton
+
+local function SetCollapsed(header, collapsed)
+	local MinimizeButton = header.MinimizeButton
+	MinimizeButton:SetNormalTexture("Interface/QuestFrame/QuestTracker")
+	MinimizeButton:SetPushedTexture("Interface/QuestFrame/QuestTracker")
+	MinimizeButton:SetHighlightAtlas("UI-QuestTrackerButton-Highlight", "ADD")
+
+	if collapsed then
+		MinimizeButton:GetNormalTexture():SetTexCoord(0.933594, 0.96875, 0.242188, 0.316406)
+		MinimizeButton:GetPushedTexture():SetTexCoord(0.894531, 0.929688, 0.324219, 0.398438)
+	else
+		MinimizeButton:GetNormalTexture():SetTexCoord(0.9375, 0.972656, 0.121094, 0.195312)
+		MinimizeButton:GetPushedTexture():SetTexCoord(0.894531, 0.929688, 0.242188, 0.316406)
 	end
-end)
+end
+
+if MinimizeButton then
+	SetCollapsed(TrackerHeader, TrackerFrame.isCollapsed)
+	hooksecurefunc(TrackerHeader, 'SetCollapsed', SetCollapsed)
+end
