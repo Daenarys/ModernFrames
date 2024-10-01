aae7475235d104d82368d9a7b72de16626407b80
Daenarys <androidwoz@gmail.com>
2024-10-02 01:03:14 +0200

    use new art

--- ModernFrames/skins/Character.lua
+++ ModernFrames/skins/Character.lua
@@ -1,10 +1,6 @@
 if not _G.CharacterFrame then return end
 
-CharacterFrame.Background:Hide()
 ReputationFrame.filterDropdown:Hide()
-
-TokenFrame.CurrencyTransferLogToggleButton:ClearAllPoints()
-TokenFrame.CurrencyTransferLogToggleButton:SetPoint("TOPRIGHT", -11, -32)
 
 if (ReputationFrame.FactionLabel == nil) then
 	ReputationFrame.FactionLabel = ReputationFrame:CreateFontString(nil, "ARTWORK", "GameFontHighlight")
@@ -16,6 +12,9 @@ if (ReputationFrame.StandingLabel == nil) then
 	ReputationFrame.StandingLabel:SetText(STANDING)
 	ReputationFrame.StandingLabel:SetPoint("TOPLEFT", 215, -42)
 end
+
+TokenFrame.CurrencyTransferLogToggleButton:ClearAllPoints()
+TokenFrame.CurrencyTransferLogToggleButton:SetPoint("TOPRIGHT", -11, -32)
 
 hooksecurefunc(CharacterFrame, "UpdatePortrait", function(self)
 	self:SetPortraitToSpecIcon()
