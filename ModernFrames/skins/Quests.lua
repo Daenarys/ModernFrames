6a1aefe68774a4ecd0e12201f7172d39cbff4125
Daenarys <androidwoz@gmail.com>
2024-10-02 20:51:06 +0200

    adjust scrollbar position

--- ModernFrames/skins/Quests.lua
+++ ModernFrames/skins/Quests.lua
@@ -6,3 +6,7 @@ QuestScrollFrame:SetPoint("TOPLEFT")
 QuestScrollFrame.ScrollBar:ClearAllPoints()
 QuestScrollFrame.ScrollBar:SetPoint("TOPLEFT", QuestScrollFrame, "TOPRIGHT", 8, -4)
 QuestScrollFrame.ScrollBar:SetPoint("BOTTOMLEFT", QuestScrollFrame, "BOTTOMRIGHT", 8, -4)
+
+QuestMapDetailsScrollFrame.ScrollBar:ClearAllPoints()
+QuestMapDetailsScrollFrame.ScrollBar:SetPoint("TOPLEFT", QuestMapDetailsScrollFrame, "TOPRIGHT", 13, 40)
+QuestMapDetailsScrollFrame.ScrollBar:SetPoint("BOTTOMLEFT", QuestMapDetailsScrollFrame, "BOTTOMRIGHT", 13, -27)
