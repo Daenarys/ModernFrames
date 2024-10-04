3c7207098eea15b6541b87b8dd69b055415caeba
Daenarys <androidwoz@gmail.com>
2024-10-04 17:29:09 +0200

    adjust golden dragon position

--- ModernFrames/skins/unitframes/TargetFrame.lua
+++ ModernFrames/skins/unitframes/TargetFrame.lua
@@ -3,7 +3,7 @@ if not _G.TargetFrame then return end
 hooksecurefunc(TargetFrame, "CheckClassification", function(self)
 	local classification = UnitClassification(self.unit)
 	local bossPortraitFrameTexture = self.TargetFrameContainer.BossPortraitFrameTexture
-	if (UnitIsBossMob(self.unit) or classification == "elite" or classification == "rareelite") then
+	if (UnitIsBossMob(self.unit) or classification == "rareelite") then
 		bossPortraitFrameTexture:SetAtlas("UI-HUD-UnitFrame-Target-PortraitOn-Boss-Gold-Winged", TextureKitConstants.UseAtlasSize)
 		bossPortraitFrameTexture:SetPoint("TOPRIGHT", 8, -7)
 	elseif (classification == "rare") then
