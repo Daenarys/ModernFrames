05ecb53bb163f0c21827427be29a0a976b743bda
Daenarys <androidwoz@gmail.com>
2024-10-04 17:26:32 +0200

    give all elites wings

--- ModernFrames/skins/unitframes/TargetFrame.lua
+++ ModernFrames/skins/unitframes/TargetFrame.lua
@@ -4,8 +4,8 @@ hooksecurefunc(TargetFrame, "CheckClassification", function(self)
 	local classification = UnitClassification(self.unit)
 	local bossPortraitFrameTexture = self.TargetFrameContainer.BossPortraitFrameTexture
 	if (UnitIsBossMob(self.unit) or classification == "elite" or classification == "rareelite") then
-		bossPortraitFrameTexture:SetAtlas("UI-HUD-UnitFrame-Target-PortraitOn-Boss-Gold", TextureKitConstants.UseAtlasSize)
-		bossPortraitFrameTexture:SetPoint("TOPRIGHT", -11, -8)
+		bossPortraitFrameTexture:SetAtlas("UI-HUD-UnitFrame-Target-PortraitOn-Boss-Gold-Winged", TextureKitConstants.UseAtlasSize)
+		bossPortraitFrameTexture:SetPoint("TOPRIGHT", 8, -7)
 	elseif (classification == "rare") then
 		bossPortraitFrameTexture:SetAtlas("ui-hud-unitframe-target-portraiton-boss-rare-silver", TextureKitConstants.UseAtlasSize)
 		bossPortraitFrameTexture:SetPoint("TOPRIGHT", -11, -8)
